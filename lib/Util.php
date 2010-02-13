09065b01de732903228a6d3cd917c28c1a2ae985
sam keen <sam.sjk@gmail.com>
2010-02-13 11:03:23 -0800

    added a digest XML method

--- lib/Util.php
+++ lib/Util.php
@@ -20,5 +20,39 @@ class Util {
         }
         fclose($handle);
     }
+    public static function file_exists_and_readable($full_file_path) {
+        if( ! file_exists($full_file_path)) {
+            throw new Exception("file does not exist: {$full_file_path}");
+        }
+        if( ! is_readable($full_file_path)) {
+            throw new Exception("file is not readable: {$full_file_path}");
+        }
+        return true;
+    }
+    public static function file_exists_and_writable($full_file_path) {
+        if( ! file_exists($full_file_path)) {
+            throw new Exception("file does not exist: {$full_file_path}");
+        }
+        if(is_readable($full_file_path)) {
+            throw new Exception("file is not readable: {$full_file_path}");
+        }
+        return true;
+    }
+    public static function digest_xml_file($full_path_to_xml) {
+        $xml_obj = null;
+        if (self::file_exists_and_readable($full_path_to_xml)) {
+            libxml_use_internal_errors(true);
+            $fail_message = "";
+            $xml_obj = simplexml_load_file($full_path_to_xml);
+            if (!$xml_obj) {
+                $fail_message = "Failed loading XML\n";
+                foreach(libxml_get_errors() as $error) {
+                    $fail_message .= "\t{$error->message}";
+                }
+                throw new Exception("Failed to contruct SimpleXML object :{$fail_message}");
+            }
+        }
+        return $xml_obj;
+    }
 }
 ?>
