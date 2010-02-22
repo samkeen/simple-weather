e975e66beb55de6d9b6928125dfdcd89be18d6af
sam keen <sam.sjk@gmail.com>
2010-02-22 08:17:48 -0800

    final tweak to cronjob to ready it fo use
    - if block to only run as cli (not from web)
    - code to call weather api with given commandline args complete

--- cron.php
+++ cron.php
@@ -1,20 +1,27 @@
 #!/usr/bin/php
 <?php
-if ($argc != 4 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
-?>
+if($argc) {
+    if ($argc != 4 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) { ?>
 
-This script uses the SimpleWeatherAPI to retrieve a given number of 24h period
-summarized forecasts for a given zipcode.
+    This script uses the SimpleWeatherAPI to retrieve a given number of 24h period
+    summarized forecasts for a given zipcode.
 
-  Usage:
-  <?php echo $argv[0]; ?> <zipcode> <startdate> <number of days to retrieve>
+      Usage:
+      <?php echo $argv[0]; ?> <zipcode> <startdate> <number of days to retrieve>
 
-  <zipcode> This is a US zipcode of 5 digits. ex: 97210
-  <startdate> is in the form of Y-m-d. ex: 2010-02-05
-  <number of days to retrieve> This is the number of days to retrieve.  It is
-      inclusive of the startdate.
-      
-  With the --help, -help, -h, or -? options, you can get this help.
-<?php }
-var_dump($argc);
-var_dump($argv);
+      <zipcode> This is a US zipcode of 5 digits. ex: 97210
+      <startdate> is in the form of Y-m-d. ex: 2010-02-05
+      <number of days to retrieve> This is the number of days to retrieve.  It is
+          inclusive of the startdate.
+
+      With the --help, -help, -h, or -? options, you can get this help.
+    <?php } else {
+        require "./lib/WeatherApi.php";
+        $zipcode = $argv[1];
+        $start_date = strtotime($argv[2]);
+        $days_to_collect = (int)$argv[3];
+        $weather = new WeatherApi();
+        echo "\nMaking call: get_24h_periods($zipcode,$start_date,$days_to_collect)\n";
+        $weather->get_24h_periods($zipcode,$start_date,$days_to_collect);
+    }
+}
